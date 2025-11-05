e645ce6034a20c93e801c2c999e1507b7d5227de
mwm <maciej.w.majewski@gmail.com>
2025-11-05 12:26:34 +0100

    new room added

--- README.md
+++
@@ -1,3 +0,0 @@
-```bash
-source /home/dokt/majewski/Public/ml_env_206/.venv/bin/activate
-```
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ml-env-206!")
-
-
-if __name__ == "__main__":
-    main()
